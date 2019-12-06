da048025dcb9d2aef42fe215ff4ed24a4921ab8a
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2019-12-06 12:49:09 -0300

    version 3

---
+++ app/src/main/java/com/alvitre/acolher/MainActivity.java
@@ -0,0 +1,22 @@
+package com.alvitre.acolher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("http://www.acolher.alvitre.net/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
